1e58d51fa38295c6a2b7050642da734531cf7900
jbechcas <bechtt200@gmail.com>
2023-11-28 21:03:46 +0100

    subida

--- CAP06EJ01/src/Ejercicio29.java
+++ CAP06EJ01/src/Ejercicio29.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-
+//(max-desplazamiento+1)) + desplazamiento;
 public class Ejercicio29 {
     public static void main(String[] args) {
         System.out.println("1. Primavera\r\n" + //
@@ -11,11 +11,78 @@ public class Ejercicio29 {
         int estacion = sc.nextInt();
         switch (estacion) {
             case 1:
+                //Primavera
+                int tempMax = (int)(Math.random()*(30-15+1))+15;
+                int tempMin = (int)(Math.random()*(tempMax-15+1))+15;
+                System.out.println("Previsión del tiempo para mañana");
+                System.out.println("---------------------------------");
+                System.out.println("Temperatura máxima: " + tempMax);
+                System.out.println("Temperatura mínima: " + tempMin);
+                int nubladoSoleado = (int)(Math.random()*10)+1;
+                String nuString = "";
+                if (nubladoSoleado<=6){
+                    nuString = "soleado";
+                }else{
+                    nuString = "nublado";
+                }
+                System.out.println(nuString);
                 
                 break;
-        
-            default:
+            case 2:
+                //Verano
+                tempMax = (int)(Math.random()*(45-25+1))+25;
+                tempMin = (int)(Math.random()*(tempMax-25+1))+25;
+                System.out.println("Previsión del tiempo para mañana");
+                System.out.println("---------------------------------");
+                System.out.println("Temperatura máxima: " + tempMax);
+                System.out.println("Temperatura mínima: " + tempMin);
+                nubladoSoleado = (int)(Math.random()*10)+1;
+                nuString = "";
+                if (nubladoSoleado<=8){
+                    nuString = "soleado";
+                }else{
+                    nuString = "nublado";
+                }
+                System.out.println(nuString);
+                
                 break;
+            case 3:
+            //Otoño
+                tempMax = (int)(Math.random()*(30-20+1))+20;
+                tempMin = (int)(Math.random()*(tempMax-20+1))+20;
+                System.out.println("Previsión del tiempo para mañana");
+                System.out.println("---------------------------------");
+                System.out.println("Temperatura máxima: " + tempMax);
+                System.out.println("Temperatura mínima: " + tempMin);
+                nubladoSoleado = (int)(Math.random()*10)+1;
+                nuString = "";
+                if (nubladoSoleado<=4){
+                    nuString = "soleado";
+                }else{
+                    nuString = "nublado";
+                }
+                System.out.println(nuString);
+                
+                break;
+            case 4:
+            //Invierno
+                tempMax = (int)(Math.random()*(25+1));
+                tempMin = (int)(Math.random()*(tempMax+1));
+                System.out.println("Previsión del tiempo para mañana");
+                System.out.println("---------------------------------");
+                System.out.println("Temperatura máxima: " + tempMax);
+                System.out.println("Temperatura mínima: " + tempMin);
+                nubladoSoleado = (int)(Math.random()*10)+1;
+                nuString = "";
+                if (nubladoSoleado<=2){
+                    nuString = "soleado";
+                }else{
+                    nuString = "nublado";
+                }
+                System.out.println(nuString);
+                
+                break;
+            
         }
         sc.close();
     }
